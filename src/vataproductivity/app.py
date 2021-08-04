820ecb79147ac46f6e19b19b66399c01e464d009
sandmani <nicolai.sandmann@student.uni-augsburg.de>
2021-08-04 21:36:09 +0200

    #noissue added get_activities

--- src/vataproductivity/app.py
+++ src/vataproductivity/app.py
@@ -6,6 +6,7 @@ TODO next button to go to next task (better drop down)
 TODO Overview with tasks + time spent
 TODO Setup DB on startup if not already set up
 """
+
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
@@ -30,9 +31,25 @@ class VataProductivity(toga.App):
         print("Activity stored in db {0}: ".format(c.fetchone()))
         conn.close()
         
+        
+    def get_activities(self, widget):
+        package_dir = os.path.abspath(os.path.dirname(__file__))
+        db_dir = os.path.join(package_dir, 'task.db')
+        conn = sqlite3.connect(db_dir)
+        c = conn.cursor()
+        c.execute("SELECT * FROM tasks")
+        print("Activities fetched: {0}".format(c.fetchall()))
+        self.activity_label.text = "{0}".format(c.fetchall())
+        conn.close()
+        
 
     def startup(self):
         main_box = toga.Box(style=Pack(direction=COLUMN))
+        
+        self.activity_label = toga.Label(
+            'Activities name:',
+            style=Pack(padding=(0, 5))
+        )
 
         name_label = toga.Label(
             'Activity name:',
@@ -44,14 +61,22 @@ class VataProductivity(toga.App):
         name_box.add(name_label)
         name_box.add(self.name_input)
 
-        button = toga.Button(
+        add_activity_button = toga.Button(
             'Add new activity',
             on_press=self.save_activity,
             style=Pack(padding=5)
         )
+        
+        get_activities_button = toga.Button(
+            'See all activities',
+            on_press=self.get_activities,
+            style=Pack(padding=5)
+        )
 
         main_box.add(name_box)
-        main_box.add(button)
+        main_box.add(add_activity_button)
+        main_box.add(get_activities_button)
+        main_box.add(self.activity_label)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
--- src/vataproductivity/sqLiteSetup.py
+++ src/vataproductivity/sqLiteSetup.py
@@ -9,6 +9,10 @@ c.execute("""CREATE TABLE tasks
     name text PRIMARY KEY,
     time integer
     )""")
+# c.execute("INSERT INTO tasks VALUES ('testName', 0)")
+# c.execute("SELECT * FROM tasks WHERE name='testName'")
+# print(c.fetchone())
+# c.execute("DROP TABLE tasks")
 
 conn.commit()
 
